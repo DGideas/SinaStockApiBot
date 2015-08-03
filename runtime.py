f3fae6d820196cd934ee528ea079b6a1c7068cd4
王万霖 <dgideas@outlook.com>
2015-08-03 23:26:00 +0800

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 __author__='DGideas';
+import os;
+import sys;
+import codecs;
+import urllib.request;
+
 try:
 	os.chdir(os.path.dirname(sys.argv[0]));
 except FileNotFoundError:
@@ -7,13 +12,10 @@ except FileNotFoundError:
 except OSError:
 	pass;
 
-import sys;
-import codecs;
-import urllib.request;
-
 f=codecs.open('runtime.csv','a','utf8');
 i=1
 while i<=2000:
     info=urllib.request.urlopen('http://hq.sinajs.cn/list=sz'+str(i)).read();
     f.write(info+'\n');
+    print(i);
     i=i+1;
